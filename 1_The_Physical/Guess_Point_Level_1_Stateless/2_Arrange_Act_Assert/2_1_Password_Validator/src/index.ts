71f6b697afd4393eef7830c8a77e7852e8ee5e45
Mark <mark@influencer.com>
2023-04-11 14:44:11 +0200

    [Tidy] Add missing semicolons

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -2,9 +2,9 @@ const UPPERCASE_REGEX = /[A-Z]/g;
 const DIGIT_REGEX = /\d/g;
 const validatePassword = (password: string) => {
     const errors: string[] = []
-    if (password.length < 5 || password.length > 15) errors.push('PWD_LENGTH')
-    if (!UPPERCASE_REGEX.test(password)) errors.push('NO_UPPERCASE')
-    if (!DIGIT_REGEX.test(password)) errors.push('NO_DIGIT')
+    if (password.length < 5 || password.length > 15) errors.push('PWD_LENGTH');
+    if (!UPPERCASE_REGEX.test(password)) errors.push('NO_UPPERCASE');
+    if (!DIGIT_REGEX.test(password)) errors.push('NO_DIGIT');
     return {
         isValid: errors.length === 0,
         errors 
