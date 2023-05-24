080e818267056efa0696ea7c2b012a0fd8ee8ce4
Mark <mark@influencer.com>
2023-05-24 16:35:31 +0400

    [New] Knows that input "01:12 - 14:32" is valid military time

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -20,6 +20,7 @@ describe('military time validator', () => {
             ['24:39 - 20:42', 'invalid', false],
             ['22:29 - 00:61', 'invalid', false],
             ['-2:29 - 00:-1', 'invalid', false],
+            ['01:12 - 14:32', 'valid', true],
         ])('knows that input "%s" is %s military time', (input, _, expected) => {
             const output = validateMilitaryTime(input);
             expect(output).toBe(expected);
