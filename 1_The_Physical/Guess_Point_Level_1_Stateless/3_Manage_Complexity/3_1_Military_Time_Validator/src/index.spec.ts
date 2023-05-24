f0bc6083efc82affeee3e47ed848c9c730543ba6
Mark <mark@influencer.com>
2023-05-24 15:39:36 +0400

    [New] Knows when input "05:39 - 20:42" is in valid time-range format

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { validateMilitaryTime } from './index';
 
 describe('military time validator', () => {
-
-
+    it('knows when input is "05:39 - 20:42" in valid time-range format', () => {
+        const output = validateMilitaryTime('05:39 - 20:42');
+        expect(output).toBeTruthy();
+    })
 })
