e30b2a970e4a4f0d7c4772df13de44444a8b512a
Mark <mark@influencer.com>
2023-05-24 16:02:46 +0400

    [Chore] spelling

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,7 +5,7 @@ describe('military time validator', () => {
         ['05:39 - 20:42', 'valid', true],
         ['3:20-20:42', 'invalid', false],
         ['05:39---20:42', 'invalid', false],
-    ])('knows that input is "%s" in %s time-range format', (input, _, expected) => {
+    ])('knows that input "%s" is in %s time-range format', (input, _, expected) => {
         const output = validateMilitaryTime(input);
         expect(output).toBe(expected);
     })
