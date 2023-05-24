cd032768f9c6f18b43a8ce1bba6202bb1ff03ef4
Mark <mark@influencer.com>
2023-05-24 16:01:16 +0400

    [Refactor] Remove duplication

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,16 +1,12 @@
 import { validateMilitaryTime } from './index';
 
 describe('military time validator', () => {
-    it('knows when input is "05:39 - 20:42" in valid time-range format', () => {
-        const output = validateMilitaryTime('05:39 - 20:42');
-        expect(output).toBeTruthy();
-    })
-    it('knows when input "3:20-20:42" is in invalid time-range format', () => {
-        const output = validateMilitaryTime('3:20-20:42');
-        expect(output).toBeFalsy();
-    })
-    it('knows when input "05:39---20:42" is in invalid time-range format', () => {
-        const output = validateMilitaryTime('05:39---20:42');
-        expect(output).toBeFalsy();
+    it.each([
+        ['05:39 - 20:42', 'valid', true],
+        ['3:20-20:42', 'invalid', false],
+        ['05:39---20:42', 'invalid', false],
+    ])('knows that input is "%s" in %s time-range format', (input, _, expected) => {
+        const output = validateMilitaryTime(input);
+        expect(output).toBe(expected);
     })
 })
