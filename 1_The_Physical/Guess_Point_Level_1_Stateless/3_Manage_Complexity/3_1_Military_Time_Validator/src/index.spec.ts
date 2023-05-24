22b197113f592657171a2c0170f9b9dd355208ad
Mark <mark@influencer.com>
2023-05-24 16:33:20 +0400

    [Refactor] Remove duplication

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,28 +1,28 @@
 import { validateMilitaryTime } from './index';
 
 describe('military time validator', () => {
-    it.each([
-        ['05:39 - 20:42', 'valid', true],
-        ['20:30 - 02:15', 'valid', true],
-        ['3:20-20:42', 'invalid', false],
-        ['05:39---20:42', 'invalid', false],
-        ['abc', 'invalid', false],
-        ['ab:cd - ef:gh', 'invalid', false],
-    ])('knows that input "%s" is in %s time-range format', (input, _, expected) => {
-        const output = validateMilitaryTime(input);
-        expect(output).toBe(expected);
+    describe('validates time range format', () => {
+        it.each([
+            ['05:39 - 20:42', 'valid', true],
+            ['20:30 - 02:15', 'valid', true],
+            ['3:20-20:42', 'invalid', false],
+            ['05:39---20:42', 'invalid', false],
+            ['abc', 'invalid', false],
+            ['ab:cd - ef:gh', 'invalid', false],
+        ])('knows that input "%s" is in %s time-range format', (input, _, expected) => {
+            const output = validateMilitaryTime(input);
+            expect(output).toBe(expected);
+        })
     })
 
-    it('knows that input "24:39 - 20:42" is invalid military time', () => {
-        const output = validateMilitaryTime('24:39 - 20:42');
-        expect(output).toBeFalsy();
-    })
-    it('knows that input "22:29 - 00:61" is invalid military time', () => {
-        const output = validateMilitaryTime('24:39 - 20:42');
-        expect(output).toBeFalsy();
-    })
-    it('knows that input "-2:29 - 00:-1" is invalid military time', () => {
-        const output = validateMilitaryTime('24:39 - 20:42');
-        expect(output).toBeFalsy();
+    describe('validates military time range', () => {
+        it.each([
+            ['24:39 - 20:42', 'invalid', false],
+            ['22:29 - 00:61', 'invalid', false],
+            ['-2:29 - 00:-1', 'invalid', false],
+        ])('knows that input "%s" is in %s military time', (input, _, expected) => {
+            const output = validateMilitaryTime(input);
+            expect(output).toBe(expected);
+        })
     })
 })
