257196919a87e4a3bba585dbf8bb60c59a56a440
Mark <mark@influencer.com>
2023-05-24 16:04:49 +0400

    [New] Knows that input "abc" is in invalid time-range format

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,6 +5,7 @@ describe('military time validator', () => {
         ['05:39 - 20:42', 'valid', true],
         ['3:20-20:42', 'invalid', false],
         ['05:39---20:42', 'invalid', false],
+        ['abc', 'invalid', false],
     ])('knows that input "%s" is in %s time-range format', (input, _, expected) => {
         const output = validateMilitaryTime(input);
         expect(output).toBe(expected);
