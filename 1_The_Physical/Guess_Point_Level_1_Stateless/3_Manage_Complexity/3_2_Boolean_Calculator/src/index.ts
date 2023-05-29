ea7d1373f0ecdba277fb2d0c99225808c6755a80
Mark <mark@influencer.com>
2023-05-29 14:56:01 +0400

    [Refactor] Remove duplication

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,17 +1,39 @@
+interface OperatorArguments {
+    left: string | null;
+    right: string| null;
+}
+
 export default class BooleanCalculator {
     private static isTrue(token: string): boolean {
         return token === 'TRUE'
     }
 
+    static getArgumentsForOperator(operator: string, expression: string): OperatorArguments {
+        const result: OperatorArguments = {
+            left: null,
+            right: null,
+        }
+        const operatorIndex = expression.indexOf(operator);
+        if (operatorIndex >= 0) {
+            result.left = expression.slice(0, operatorIndex - 1);
+            result.right = expression.slice(operatorIndex + operator.length + 1);
+        }
+        return result;
+    }
+
     static calculate(input: string): boolean {
-        const andOperatorIndex = input.indexOf('AND');
-        const orOperatorIndex = input.indexOf('OR');
-        if (input.indexOf('NOT') >= 0)
+        const notArguments = this.getArgumentsForOperator('NOT', input);
+        if (notArguments.right)
             return !this.isTrue(input.slice(4));
-        if (andOperatorIndex >= 0) 
-            return this.calculate(input.slice(0, andOperatorIndex - 1)) && this.calculate(input.slice(andOperatorIndex + 4));
-        if (orOperatorIndex >= 0) 
-            return this.isTrue(input.slice(0, orOperatorIndex - 1)) || this.isTrue(input.slice(orOperatorIndex+ 3))
+
+        const andArguments = this.getArgumentsForOperator('AND', input);
+        if (andArguments.left && andArguments.right) 
+            return this.calculate(andArguments.left) && this.calculate(andArguments.right);
+
+        const orArguments = this.getArgumentsForOperator('OR', input);
+        if (orArguments.left && orArguments.right) 
+            return this.isTrue(orArguments.left) || this.isTrue(orArguments.right)
+
         return this.isTrue(input);
     }
 }
