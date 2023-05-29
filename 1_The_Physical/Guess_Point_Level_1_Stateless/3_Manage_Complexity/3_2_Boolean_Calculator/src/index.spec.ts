5c0f7f2eeb6790bd13a220cb4f042cbdb1a25cae
Mark <mark@influencer.com>
2023-05-29 13:24:52 +0400

    [New] Able to evaluate "TRUE AND TRUE" expression

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -12,6 +12,7 @@ describe('boolean calculator', () => {
             ['NOT TRUE', false],
             ['NOT FALSE', true],
             ['TRUE AND FALSE', false],
+            ['TRUE AND TRUE', true]
         ])('is able to evaluate "%s" expression', (expression, expectedOutput) => {
             const output = BooleanCalculator.calculate(expression);
             expect(output).toBe(expectedOutput);
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,9 +1,10 @@
 export default class BooleanCalculator {
     static calculate(input: string): boolean {
+        const andOperatorIndex = input.indexOf('AND');
         if (input.indexOf('NOT') >= 0)
             return !(input.slice(4) === 'TRUE');
-        if (input.indexOf('AND') >= 0) 
-            return false
+        if (andOperatorIndex >= 0) 
+            return input.slice(0, andOperatorIndex - 1) === 'TRUE' && input.slice(andOperatorIndex + 4) === 'TRUE';
         return input === 'TRUE';
     }
 }
