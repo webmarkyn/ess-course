5d1402241e284ca82d1ce02de7c62986617d9884
Mark <mark@influencer.com>
2023-05-29 13:29:40 +0400

    [Refactor] Remove duplication

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,10 +1,14 @@
 export default class BooleanCalculator {
+    private static isTrue(token: string): boolean {
+        return token === 'TRUE'
+    }
+
     static calculate(input: string): boolean {
         const andOperatorIndex = input.indexOf('AND');
         if (input.indexOf('NOT') >= 0)
-            return !(input.slice(4) === 'TRUE');
+            return !this.isTrue(input.slice(4));
         if (andOperatorIndex >= 0) 
-            return input.slice(0, andOperatorIndex - 1) === 'TRUE' && input.slice(andOperatorIndex + 4) === 'TRUE';
-        return input === 'TRUE';
+            return this.isTrue(input.slice(0, andOperatorIndex - 1)) && this.isTrue(input.slice(andOperatorIndex + 4));
+        return this.isTrue(input);
     }
 }
